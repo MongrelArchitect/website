37da99bf655ed3f197ddd3fd73b1898e0e8a00ef
Sean Eric Thomas <set@seanericthomas.com>
2022-12-02 22:38:26 -0800

    Randomize first quote on page load & add a few more

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Keys are used to cycle through quotes on page
 const quotes = {
   0: {
     'quote': 'Those who are capable of tyranny are capable of perjury ' +
@@ -13,7 +14,7 @@ const quotes = {
   2: {
     'quote': 'The only thing that makes life possible is permanent, ' +
              'intolerable uncertainty.',
-    'author': ' - Ursula K. Le Guin',
+    'author': ' - Ursula Le Guin',
   },
 
   3: {
@@ -21,14 +22,58 @@ const quotes = {
              'less hidden.',
     'author': ' - Gene Wolfe',
   },
+
+  4: {
+    'quote': 'There can be no truly moral choice unless that choice ' +
+             'is made in freedom',
+    'author': ' - Murray Rothbard',
+  },
+
+  5: {
+    'quote': 'There are two principles between which there can be no ' +
+             'compromise\: liberty and coercion.',
+    'author': ' - Frédéric Bastiat',
+  },
+
+  6: {
+    'quote': 'Anarchy is no guarantee that some people won\'t kill, ' +
+             'injure, kidnap, defraud, or steal from others. Government ' +
+             'is a guarantee that some will.',
+    'author': ' - Gustave de Molinari',
+  },
+
+  7: {
+    'quote': 'He who is unfit to serve his fellow citizens wants to rule them',
+    'author': ' - Ludwig von Mises',
+  },
+
+  8: {
+    'quote': 'Part of freedom is the right of each of us to go to hell ' +
+              'in his own fashion.',
+    'author': ' - David D. Friedman',
+  },
+
+  9: {
+    'quote': 'Poverty can be brought about by law; it cannot be ' +
+             'forbidden by law.',
+    'author': ' - Isabel Paterson',
+  },
 };
 
-let quoteIndex = 0;
+let quoteIndex = Math.floor(Math.random() * Object.keys(quotes).length);
+if (quoteIndex < 0) {
+  quoteIndex = 0;
+}
 
 const quote = document.querySelector('#quote-link');
+const quoteText = document.querySelector('.quote');
+const quoteAuthor = document.querySelector('.author');
+
+quoteText.textContent = quotes[quoteIndex].quote;
+quoteAuthor.textContent = quotes[quoteIndex].author;
+
 quote.addEventListener('click', () => {
-  const quoteText = document.querySelector('.quote');
-  const quoteAuthor = document.querySelector('.author');
+  // Cycle through the quotes
   if (quoteIndex < Object.keys(quotes).length - 1) {
     quoteIndex += 1;
     quoteText.textContent = quotes[quoteIndex].quote;
