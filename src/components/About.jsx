5e052cb04ab632e5bfa05f5c21524b2f9986fe70
Sean Eric Thomas <set@seanericthomas.com>
2023-06-13 23:21:36 -0700

    Setup portfolio page with about section

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <article className="about">
+      <h1>Sean Eric Thomas</h1>
+      <div className="headshot" />
+      <p>
+        I&apos;m a web developer from southern California. I like
+        guitar, reading and hiking with my kids. I am good at computer.
+      </p>
+    </article>
+  );
+}
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Nav from './Nav';
+import About from './About';
 
 export default function Portfolio() {
   return (
-    <div />
+    <main className="portfolio">
+      <Nav />
+      <div className="info">
+        <About />
+      </div>
+    </main>
   );
 }
