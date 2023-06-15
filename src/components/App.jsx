9339903095b345616acb3b78cf485dc1b306e865
Sean Eric Thomas <set@seanericthomas.com>
2023-06-15 15:03:14 -0700

    Save current quote and background to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,20 @@ import backgrounds from '../util/backgrounds';
 import quotes from '../util/quotes';
 
 export default function App() {
-  const [backgroundImage, setBackgroundImage] = useState(0);
-  const [currentQuote, setCurrentQuote] = useState(0);
+  const [backgroundImage, setBackgroundImage] = useState(
+    +localStorage.getItem('background') || 0,
+  );
+  const [currentQuote, setCurrentQuote] = useState(
+    +localStorage.getItem('quote') || 0,
+  );
 
   const changeBackground = (event) => {
     if (backgroundImage === backgrounds.length - 1) {
       setBackgroundImage(0);
+      localStorage.setItem('background', 0);
     } else {
       setBackgroundImage(backgroundImage + 1);
+      localStorage.setItem('background', backgroundImage + 1);
     }
     event.target.parentElement.blur();
   };
@@ -19,8 +25,10 @@ export default function App() {
   const changeQuote = (event) => {
     if (currentQuote === quotes.length - 1) {
       setCurrentQuote(0);
+      localStorage.setItem('quote', 0);
     } else {
       setCurrentQuote(currentQuote + 1);
+      localStorage.setItem('quote', currentQuote + 1);
     }
     event.target.parentElement.blur();
   };
