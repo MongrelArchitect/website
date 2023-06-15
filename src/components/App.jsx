a5577df0c381958edadf08fa0f94cc28052121d2
Sean Eric Thomas <set@seanericthomas.com>
2023-06-14 22:32:41 -0700

    Add some more quotes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,6 +23,43 @@ const quotes = [
       + ' intolerable uncertainty.',
     author: 'Ursula Le Guin',
   },
+  {
+    text: 'There can be no truly moral choice unless that choice '
+          + ' is made in freedom.',
+    author: 'Murray Rothbard',
+  },
+  {
+    text: 'He who is unfit to serve his fellow citizens wants to rule them.',
+    author: 'Ludwig von Mises',
+  },
+  {
+    text: 'There is no magic. There is only knowledge, more or less hidden.',
+    author: 'Gene Wolfe',
+  },
+  {
+    text: 'Poverty can be brought about by law; it cannot be forbidden by law.',
+    author: 'Isabel Paterson',
+  },
+  {
+    text: 'Part of freedom is the right of each of us to go to hell'
+          + ' in his own fashion.',
+    author: 'David D. Friedman',
+  },
+  {
+    text: 'Government is the great fiction through which everybody '
+          + ' endeavors to live at the expense of everybody else.',
+    author: 'Frédéric Bastiat',
+  },
+  {
+    text: 'Living is fighting for life, and when anyone does not know'
+          + ' this fact, someone else is doing his fighting for him.',
+    author: 'Rose Wilder Lane',
+  },
+  {
+    text: 'Just as war is the natural consequence of monopoly, peace'
+          + ' is the natural consequence of liberty.',
+    author: 'Gustave de Molinari',
+  },
 ];
 
 export default function App() {
